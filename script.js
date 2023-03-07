eca9319aee256b8ffefe510bd0b463bad9da1d22
FromRahulRai <114037677+FromRahulRai@users.noreply.github.com>
2023-03-07 09:50:58 -0800

    Add initial files

---
+++ script.js
@@ -0,0 +1,30 @@
+let button = document.querySelector("button")
+let body = document.querySelector("body")
+const clr = ['#ff7f50', '#87cefa', '#da70d6', '#32cd32', '#6495ed',
+    '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']
+
+body.style.backgroundColor = "black"
+
+button.addEventListener('click', changeBg)
+
+function changeBg() {
+    const colorIndex = parseInt(Math.random() * clr.length)
+    body.style.backgroundColor = clr[colorIndex]
+}
+
+button.addEventListener('click', changeBt)
+
+function changeBt() {
+    const btIndex = parseInt(Math.random() * clr.length)
+    button.style.backgroundColor = clr[btIndex]
+
+}
